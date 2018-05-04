bfcc6eeea622cc4b21be76f6e61fe4db6342b8bc
Anamul Islam <anamul.islam127@gmail.com>
2018-05-04 09:20:46 -0400

    dsgdsfg

---
+++ Lab10Classes/Circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10Classes
+{
+    class Circle
+    {
+        private double radius;
+
+        public double Radius { get => radius; set => radius = value; }
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+        public double CalculateCircumfrence()
+        {
+            return Radius * Math.PI * 2;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(Radius * Math.PI * 2);
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius; 
+        }
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(Math.PI * Radius * Radius);
+        }
+        private string FormatNumber(double x)
+        {
+            return x.ToString("#.00"); 
+        }
+    }
+}
---
+++ Lab10Classes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greet();
+            int count = 0;
+            bool go = true;
+            while (go)
+            {
+                count++;
+
+                Console.Write("Enter a radius: ");
+                string Radius = Console.ReadLine();
+                bool convert = double.TryParse(Radius, out double radius);
+                Circle circleOne = new Circle(radius);
+
+                Console.WriteLine("Circumfrence:");
+                Console.WriteLine(circleOne.CalculateFormattedCircumference());
+                Console.WriteLine();
+                Console.WriteLine("Area:");
+                Console.WriteLine(circleOne.CalculateFormattedArea());
+                
+                Console.WriteLine("Would you like to test another circle?(y/n)");
+                string input = Console.ReadLine().ToLower();
+                if (input == "y")
+                {
+                    go = true;
+                }
+                else
+                {
+                    Console.WriteLine($"Goodbye! You have created {count} object circles.");
+                    go = false;
+                }
+            }
+        }
+        static void Greet()
+        {
+
+            Console.WriteLine("Welcome to the Circle Tester!");
+        }
+    }
+}
